f9519a011525229f314e57d7bfcadd1a33318b04
John Weis <weis.john@gmail.com>
2014-10-14 10:49:26 -0400

    faster nanp regex, named function for better stack traces

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 
 function strip(str) {
     return (str || "")
-        // remove parenthesis, dashes, dots
+        // remove parenthesis, dashes, dots, spaces
         .replace(/[\-\(\)\. ]/g, "")
         // remove leading `1` or `+1` only on strings longer than 10 digits
         .replace(/^(\+)?(1)([0-9]{10,})/, "$3")
 }
 
-var nanp = /^\(?([2-9][0-9]{2})\)?([2-9](?!11)[0-9]{2})?([0-9]{4})$/
-
-module.exports = function(str) {
+var reg = /^\(?([2-9][0-9]{2})\)?([2-9](?!11)[0-9]{2})?([0-9]{4})$/;
+function nanp(str) {
     if (!str) return false;
-    return nanp.test(strip(str));
+    return reg.test(strip(str));
 }
 
+module.exports = nanp;
 module.exports.strip = strip;
